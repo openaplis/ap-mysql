91c0402ff49fa56a32317f6e29dfb454e33be967
SidHarder <softwarenavigator@gmail.com>
2017-03-29 12:56:26 -0600

    working build object

--- src/build-ao.js
+++ src/build-ao.js
@@ -1,76 +1,14 @@
 var mysql = require('mysql')
 var _ = require('lodash')
 
-var connection = mysql.createConnection({
-  host     : '10.1.2.26',
-  user     : 'sqldude',
-  password : '123Whatsup',
-  database : 'lis',
-  multipleStatements: true,
-})
+var connection = mysql.createConnection()
 
 connection.connect()
 //var query = connection.query('Set @ClientName = \'Internal Medicine, SVH Clinic\'; CALL sidtest(@ClientName);')
 var query = connection.query('Set @MasterAccessionNo = \'17-16\'; CALL prcGetAccessionOrder(@MasterAccessionNo);')
 
-var schemaList = [
-  {
-    obj: {},
-    name: "accessionOrder",
-    tablename: "tblAccessionOrder",
-    pk: "MasterAccessionNo",
-    hasParent: false,
-    isCollection: false,
-    childCollections: [
-      { name: "panelSetOrders", tablename: "tblPanelSetOrder" },
-      { name: "specimenOrders", tablename: "tblSpecimenOrder" }
-    ]
-  },
-  {
-    obj: [],
-    name: "panelSetOrders",
-    tablename: "tblPanelSetOrder",
-    pk: "ReportNo",
-    hasParent: true,
-    isCollection: true,
-    childCollections: [
-      { name: "panelOrders", tablename: "tblPanelOrder" }
-    ]
-  },
-  {
-    obj: [],
-    name: "panelOrders",
-    tablename: "tblPanelOrder",
-    pk: "panelOrderId",
-    hasParent: true,
-    isCollection: true,
-    childCollections: [
-      { name: "testOrders", tablename: "tblTestOrder" }
-    ]
-  },
-  {
-    obj: [],
-    name: "testOrders",
-    tablename: "tblTestOrder",
-    pk: "testOrderId",
-    hasParent: true,
-    isCollection: true,
-    childCollections: []
-  },
-  {
-    obj: [],
-    name: "specimenOrders",
-    tablename: "tblSpecimenOrder",
-    pk: "specimenOrderId",
-    hasParent: true,
-    isCollection: true,
-    childCollections: [
-      { name: "aliquotOrders", tablename: "tblAliquotOrder" }
-    ]
-  }
-]
-
 var accessionOrder = {}
+var rows = []
 
 query
   .on('error', function(err) {
@@ -80,52 +18,37 @@ query
     // the field packets for the rows to follow
   })
   .on('result', function(row) {
-    if(row.constructor.name == 'RowDataPacket') {
-      handleRow(row)
-    }
+    rows.push(Object.assign({}, row))
   })
   .on('end', function() {
-    assembleObjects(schemaList[0])
-    console.log(schemaList[0].obj)
+    assembleRows()
+    console.log(accessionOrder)
   })
 
 connection.end()
 
-function handleRow(row) {
-  var result = _.find(schemaList, function (o) {
-    return o.tablename == row.tablename
+function assembleRows() {
+  accessionOrder = _.find(rows, function(o) { return o.tablename == 'tblAccessionOrder'})
+
+  accessionOrder.PanelSetOrders = _.filter(rows, function(o) { return o.tablename =='tblPanelSetOrder' })
+  if(!accessionOrder.PanelSetOrders) accessionOrder.PanelSetOrders = []
+  accessionOrder.PanelSetOrders.map(function (pso) {
+      pso.PanelOrders = _.filter(rows, function(o) { return o.tablename =='tblPanelOrder' })
+      if(!pso.PanelOrders) pso.PanelOrders = []
+      pso.PanelOrders.map(function (po) {
+        po.TestOrders = _.filter(rows, function(o) { return o.tablename =='tblTestOrder' })
+        if(!po.TestOrders) po.TestOrders = []
+      })
   })
 
-  if(result) {
-    var newObject = cloneRow(row)
-    if(result.isCollection == false) {
-      result.obj = newObject
-    } else {
-      result.obj.push(newObject)
-    }
-  }
-}
-
-function assembleObjects(schema) {
-  schema.childCollections.forEach(function (collectionSchema) {
-    if(!_.has(schema, collectionSchema.name)) {
-        schema[collectionSchema.name] = []
-    }
-    _.find(schemaList, function (childSchema) {
-      if(collectionSchema.tablename == childSchema.tablename) {
-        childSchema.obj.forEach(function (childObj) {
-          schema[collectionSchema.name].push(childObj)
-          assembleObjects(childSchema)
-         })
-       }
-    })
+  accessionOrder.SpecimenOrders = _.filter(rows, function(o) { return o.tablename =='tblSpecimenOrder' })
+  if(!accessionOrder.SpecimenOrders) accessionOrder.SpecimenOrders = []
+  accessionOrder.SpecimenOrders.map(function (so) {
+      so.AliquotOrders = _.filter(rows, function(o) { return o.tablename =='tblAliquotOrder' })
+      if(!so.AliquotOrders) so.AliquotOrders = []
+      so.AliquotOrders.map(function (alo) {
+        alo.SlideOrders = _.filter(rows, function(o) { return o.tablename =='tblSlideOrder' })
+        if(!alo.SlideOrders) alo.SlideOrders = []
+      })
   })
-}
-
-function cloneRow (row) {
-  var newObject = {}
-  for(var field in row) {
-    newObject[field]=row[field]
-  }
-  return newObject
 }

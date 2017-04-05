9d504c7dbfdfc72a5a04c9f9298ddbb955a84598
SidHarder <softwarenavigator@gmail.com>
2017-04-05 15:01:48 -0600

    working on exports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 const cmdBuilder = require('./core/cmdBuilder')
 const cmdSubmitter = require('./core/cmdSubmitter')
+const aoBuilder = require('./core/aoBuilder')
 
 exports = module.exports = {
-  cmdBuilder = cmdBuilder,
-  cmdSubmitter = cmdSubmitter
+  cmdBuilder: cmdBuilder,
+  cmdSubmitter: cmdSubmitter,
+  aoBuilder: aoBuilder
 }

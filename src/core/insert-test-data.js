0416691e011ac4ae831f890a8c7c70006d4cf654
SidHarder <softwarenavigator@gmail.com>
2017-05-10 11:04:57 -0600

    moving test data

---
+++ src/core/insert-test-data.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const apObject = require('ap-object')
+// load the test-data from ap-object
+// then create clones that are blandk
+// then use insert command builder to create the insert commands.
---
+++ test/insert-test-data-test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const assert = require('chai').assert
+const path = require('path')
+const insertTestData = require(path(__dirname, '../src/core/insert-test-data'))
+
+describe('insert test data tests', function() {
+  it('insert client data', function(done) {
+    
+    done()
+  })
+}

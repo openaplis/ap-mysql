c48b50ea50c0bf4be35a6cf4fdea8e8fbcb1a99e
SidHarder <softwarenavigator@gmail.com>
2017-06-29 08:14:20 -0600

    removing as service

--- src/start-service.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-var fs = require('fs')
-
-var mysqlService = require(path.join(__dirname, './core/mysql-service'))
-
-mysqlService.start(function (err, message) {
-  if(err) return console.log(err)
-  console.log(message)
-})

537cfd50c2deb8f57db9db7d280a17b6b81a191b
SidHarder <softwarenavigator@gmail.com>
2018-02-08 13:44:53 -0700

    version

--- test/ao-retriever-test.js
+++ test/ao-retriever-test.js
@@ -7,7 +7,7 @@ describe('ao-retriever', function() {
   it('Retrieve', function(done) {
     aoRetriever.retrieve('17-123', function (err, ao) {
       if(err) return console.log(err)
-      assert.equal(ao.accessionOrder.masterAccessionNo, '17-123', 'the master accessionno is not correct')
+      assert.equal(ao.accessionOrder.masterAccessionNo, '17-123', 'the master accessionno is not correct')      
       done()
     })
   })
